d43875808a5b8d7fe1f74210fc1b5d6a97c30513
RohithKonanRavi <x23195983@student.ncirl.ie>
2024-04-21 11:21:07 +0100

    changes in the tracks url and also the users views.py code

--- tracking/urls.py
+++ tracking/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('tracking-record', views.trackinglisting, name="trackinglisting"),
     path('tracking-search', views.trackingsearch, name="trackingsearch"),
-    re_path(r'^tracking-add/(?P<goodsId>\w{0,50})/$', views.add, name="add"),
-    re_path(r'^tracking-update/(?P<trackingId>\w{0,50})/$', views.update, name="update"),
+    re_path(r'^tracking-add/(?P<goodsId>[\w-]{0,50})/$', views.add, name="add"),
+    re_path(r'^tracking-update/(?P<trackingId>[\w-]{0,50})/$', views.update, name="update"),
     re_path(r'^tracking-delete/(?P<id>\w{0,50})/$', views.delete, name="delete"),
     # re_path(r'^tracking-delete/(?P<trackingId>\w{0,50})/$', views.delete, name="delete"),
 ]
