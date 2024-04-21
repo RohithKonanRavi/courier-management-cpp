26cde5e43b98666ba40400940d9c7682d0cc269e
RohithKonanRavi <x23195983@student.ncirl.ie>
2024-04-21 13:20:56 +0100

    new chagnes

--- goods/urls.py
+++ goods/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name="login"),
     path('goods-record', views.goodslisting, name="goodslisting"),
     path('goods-add', views.add, name="add"),
-    re_path(r'^update/(?P<goodsId>\w{0,50})/$', views.update, name="update"),
+    re_path(r'^update/(?P<goodsId>[0-9a-f-]{36})/$', views.update, name='update'),
     # re_path(r'^delete/(?P<goods_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.delete_goods, name="delete_goods"),
-    re_path(r'^delete/(?P<goods_id>[0-9a-f]{32})/$', views.delete_goods, name="delete_goods"),
-]
+    re_path(r'^delete/(?P<goods_id>[0-9a-f-]{36})/$', views.delete_goods, name="delete_goods"),
+]   
